05f25ca020a1c3cb814ad94fa05957e886aaeaba
0xfr34ky <->
2020-05-06 16:07:46 +0200

    fix env variable access

--- app/translatorApp.js
+++ app/translatorApp.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '${TELEGRAM_BOT_TOKEN}';
+const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
