b8d0f90fd9d4dbe0bb39913cc6153d995c4d4695
0xfr34ky <->
2020-05-08 11:07:44 +0200

    Correctly implement echo with telegram api

--- app/translatorApp.js
+++ app/translatorApp.js
@@ -1,5 +1,11 @@
 var http = require('http');
 
+function sendMessage(chat_id, message){
+	var url = "https://api.telegram.org/bot"+process.env.TELEGRAM_BOT_TOKEN+"/sendmessage?text="+message+"&chat_id="+chat_id;
+	https = require('https');
+	https.get(url);
+}
+
 var server = http.createServer(function (request, response) {
 	if(request.method === 'POST'){
 		let body = '';
@@ -9,15 +15,17 @@ var server = http.createServer(function (request, response) {
 		request.on('end', () => {
 			try {
 				var obj = JSON.parse(body);
-				var text = obj.message.text;
-        text.split(" ");
-        if(text[0] === '/echo'){
-          text.shift;
-          console.log(text.join(" "));
-        }
+				var text = obj.message.text.split(" ");
+				var chatID = obj.message.chat.id;
 			} catch(err) {
 				console.log('no JSON');
+				response.end("ERR");
 			}
+        if(text[0] == "/echo"){
+		message = text.join(text.shift());
+		console.log(message);
+		sendMessage(chatID, message);
+        }
 		});
 	}
 	response.writeHead(200, {"Content-Type": "text/plain"});
