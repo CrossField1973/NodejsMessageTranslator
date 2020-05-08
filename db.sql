801f2893a584b270bf13d017a569228d9ae4cd47
0xfr34ky <->
2020-05-08 18:21:47 +0200

    Add database schema

---
+++ db.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `ttranslate.users` (
+`telegram_id` BIGINT NOT NULL,
+`input` VARCHAR(3) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL,
+`output` VARCHAR(3) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL,
+`created_at` INT NOT NULL,
+`updated_at` INT NOT NULL,
+KEY `by_telegram_id` (`telegram_id`) USING BTREE,
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
