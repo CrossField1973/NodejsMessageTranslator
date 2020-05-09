5ccef26f8ae3497f3af6717c2c3b8117073f7924
0xfr34ky <->
2020-05-09 09:47:06 +0200

    Update SQL

--- db.sql
+++ db.sql
@@ -1,9 +1,10 @@
-CREATE TABLE `ttranslate.users` (
+﻿USE ttranslate;
+CREATE TABLE `users` (
 `telegram_id` BIGINT NOT NULL,
 `input` VARCHAR(3) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL,
 `output` VARCHAR(3) CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL,
 `created_at` INT NOT NULL,
 `updated_at` INT NOT NULL,
 KEY `by_telegram_id` (`telegram_id`) USING BTREE,
-PRIMARY KEY (`id`)
+PRIMARY KEY (`telegram_id`)
 ) ENGINE=MyISAM;
